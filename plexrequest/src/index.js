029ce8f68ff148fd1efaa69ca43d5705edc8e1c5
Danek <danek.fill@outlook.com>
2023-06-07 12:00:59 -0400

    returning to react scripts 5.0.1 to use integrated dotenv available

--- plexrequest/src/index.js
+++ plexrequest/src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-require('dotenv').config();
-
-console.log(process.env)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
